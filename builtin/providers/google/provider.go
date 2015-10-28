744cb5ec7571c1f3d972a0efe170c617fcc1f0eb
pat <pat@22acacia.com>
2015-10-28 10:57:33 -0700

    remove experimental dataflow resource for a clean pubsub PR

--- builtin/providers/google/provider.go
+++ builtin/providers/google/provider.go
@@ -51,7 +51,6 @@ func Provider() terraform.ResourceProvider {
 			"google_compute_vpn_gateway":            resourceComputeVpnGateway(),
 			"google_compute_vpn_tunnel":             resourceComputeVpnTunnel(),
 			"google_container_cluster":              resourceContainerCluster(),
-			"google_dataflow":                       resourceDataflow(),
 			"google_dns_managed_zone":               resourceDnsManagedZone(),
 			"google_dns_record_set":                 resourceDnsRecordSet(),
 			"google_compute_instance_group_manager": resourceComputeInstanceGroupManager(),
