9642bd598c85bb940d85eb63863b9431db75e3c3
pat <pat@22acacia.com>
2015-11-12 23:21:56 -0800

    initial commit.
    this is not the long term path.  there is a golang API but that
    work is being done in another massive PR that I can't wait for

---
+++ builtin/providers/google/resource_container_replica_controller.go
@@ -0,0 +1,130 @@
+package google
+
+import (
+	"fmt"
+	"github.com/22acacia/terraform-gcloud"
+	"github.com/hashicorp/terraform/helper/schema"
+)
+
+func resourceReplicaController() *schema.Resource {
+	return &schema.Resource{
+		Create: resourceReplicaControllerCreate,
+		Read:   resourceReplicaControllerRead,
+		Delete: resourceReplicaControllerDelete,
+
+		Schema: map[string]*schema.Schema{
+			"name": &schema.Schema{
+				Type:     schema.TypeString,
+				Required: true,
+				ForceNew: true,
+			},
+
+			"docker_image": &schema.Schema{
+				Type:     schema.TypeString,
+				Required: true,
+				ForceNew: true,
+			},
+
+			"container_name": &schema.Schema{
+				Type:     schema.TypeString,
+				Required: true,
+				ForceNew: true,
+			},
+
+			"zone": &schema.Schema{
+				Type:     schema.TypeString,
+				Required: true,
+				ForceNew: true,
+			},
+
+			"optional_args": &schema.Schema{
+				Type:     schema.TypeMap,
+				Optional: true,
+				ForceNew: true,
+				Elem:	  schema.TypeString,
+			},
+		},
+	}
+}
+
+func cleanOptionalArgs(optional_args map[string]interface{}) map[string]string {
+	cleaned_opts := make(map[string]string)
+	for k,v := range  optional_args {
+		cleaned_opts[k] = v.(string)
+	}
+	return cleaned_opts
+}
+
+func resourceReplicaControllerCreate(d *schema.ResourceData, meta interface{}) error {
+	config := meta.(*Config)
+        err := terraformGcloud.InitGcloud(config.AccountFile)
+	if err != nil {
+		return err
+	}
+
+	err = terraformGcloud.InitKubectl(d.Get("container_name").(string), d.Get("zone").(string)
+	if err != nil {
+		return err
+	}
+
+	optional_args := cleanOptionalArgs(d.Get("optional_args").(map[string]interface{}))
+	uid, err := terraformGcloud.CreateRC(d.Get("name").(string), d.Get("docker_image").(string), optional_args)
+	if err != nil {
+		return err
+	}
+
+	d.SetId(uid)
+
+	err = resourceReplicaControllerRead(d, meta)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func resourceReplicaControllerRead(d *schema.ResourceData, meta interface{}) error {
+	config := meta.(*Config)
+        err := terraformGcloud.InitGcloud(config.AccountFile)
+	if err != nil {
+		return err
+	}
+
+	err = terraformGcloud.InitKubectl(d.Get("container_name").(string), d.Get("zone").(string)
+	if err != nil {
+		return err
+	}
+
+	pod_count, err := terraformGcloud.ReadRC(d.Get("name").(string))
+	if err != nil {
+		return err
+	}
+
+	if pod_count == 0 {
+		//  something has gone awry, there should always be at least one pod
+		log.Printf("There are no pods associated with this Replica Controller.  This is unexpected and probably wrong.  Please investigate")
+	}
+
+	return nil
+}
+
+func resourceReplicaControllerDelete(d *schema.ResourceData, meta interface{}) error {
+	config := meta.(*Config)
+        err := terraformGcloud.InitGcloud(config.AccountFile)
+	if err != nil {
+		return err
+	}
+
+	err = terraformGcloud.InitKubectl(d.Get("container_name").(string), d.Get("zone").(string)
+	if err != nil {
+		return err
+	}
+
+	err := terraformGcloud.DeleteRC(d.Get("name").(string),) 
+	if err != nil {
+		return err
+	}
+
+	d.SetId("")
+	return nil
+}
