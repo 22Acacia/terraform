a88a69c6bad496208adebb82e02c786cd3333424
pat <pat@22acacia.com>
2015-12-01 21:27:36 -0800

    adds a couple more config values
    - ackDeadlineSeconds: number of seconds to wait for an ack
    - pushAttributes: attributes of a push subscription
    - pushEndpoint: target for a push subscription

--- builtin/providers/google/resource_pubsub_subscription.go
+++ builtin/providers/google/resource_pubsub_subscription.go
@@ -19,6 +19,25 @@ func resourcePubsubSubscription() *schema.Resource {
 				ForceNew: true,
 			},
 
+			"ackDeadlineSeconds": &schema.Schema{
+				Type:     schema.TypeInt,
+				Optional: true,
+				ForceNew: true,
+			},
+
+			"pushAttributes": &schema.Schema{
+				Type:     schema.TypeMap,
+				Optional: true,
+				ForceNew: true,
+				Elem:     schema.TypeString,
+			},
+
+			"pushEndpoint": &schema.Schema{
+				Type:     schema.TypeString,
+				Optional: true,
+				ForceNew: true,
+			},
+
 			"topic": &schema.Schema{
 				Type:     schema.TypeString,
 				Required: true,
@@ -29,12 +48,36 @@ func resourcePubsubSubscription() *schema.Resource {
 	}
 }
 
+func cleanAdditionalArgs(args map[string]interface{}) map[string]string {
+	cleaned_args := make(map[string]string)
+	for k,v := range  args {
+		cleaned_args[k] = v.(string)
+	}
+	return cleaned_args
+}
+
 func resourcePubsubSubscriptionCreate(d *schema.ResourceData, meta interface{}) error {
 	config := meta.(*Config)
 
 	name := fmt.Sprintf("projects/%s/subscriptions/%s", config.Project, d.Get("name").(string))
 	computed_topic_name := fmt.Sprintf("projects/%s/topics/%s", config.Project, d.Get("topic").(string))
-	subscription := &pubsub.Subscription{Topic: computed_topic_name}
+
+	//  process optional parameters
+	var ackDeadlineSeconds int64
+	ackDeadlineSeconds = 10
+	if  d.Get("ackDeadlineSeconds") != "" {
+		ackDeadlineSeconds = int64(d.Get("ackDeadlineSeconds").(int))
+	}
+
+	var subscription *pubsub.Subscription
+	if d.Get("pushEndpoint").(string) != "" {
+		pushAttributes := d.Get("pushAttributes").(map[string]interface{})
+		pushAttributesClean := cleanAdditionalArgs(pushAttributes)
+		pushConfig := &pubsub.PushConfig{Attributes: pushAttributesClean, PushEndpoint: d.Get("pushEndpoint").(string)}
+		subscription = &pubsub.Subscription{AckDeadlineSeconds: ackDeadlineSeconds, Topic: computed_topic_name, PushConfig: pushConfig}
+	}  else {
+		subscription = &pubsub.Subscription{AckDeadlineSeconds: ackDeadlineSeconds, Topic: computed_topic_name}
+	}
 
 	call := config.clientPubsub.Projects.Subscriptions.Create(name, subscription)
 	res, err := call.Do()
